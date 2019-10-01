bd49b435d29d3742e6c70f10cb1f88fdf3fb3a5f
Subhankr12 <subhankr12@gmail.com>
2019-10-01 16:44:14 +0530

    message box border

--- node_modules/sequelize/types/lib/utils/logger.d.ts
+++ node_modules/sequelize/types/lib/utils/logger.d.ts
@@ -11,12 +11,8 @@ export interface LoggerConfig {
 
 export class Logger {
   constructor(config: LoggerConfig)
-  public deprecate(message: string): void;
   public debug(message: string): void;
   public warn(message: string): void;
-  public debugContext(message: string): (message: string) => void;
 }
 
-export function deprecate(message: string): void;
-export function warn(message: string): void;
-export function getLogger(): Logger;
+export const logger: Logger;
